7aff410331b4e7659dea8760aca9d36871ed7200
Natnaelk <40942186+Natnaelk@users.noreply.github.com>
2021-07-31 09:19:15 -0500

    Revert "commit on master"
    This reverts commit aca26e902b5a8acd5d994d45519be3eb23de6cd3.

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,22 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import '../screens/splash/splash_screen.dart';
 import '../routes.dart';
 import 'package:das_app/theme.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
-
+  // This widget is the root of the application.
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Das App',
+      title: 'Flutter Demo',
       theme: theme(),
       initialRoute: SplashScreen.routeName,
       routes: routes,
     );
   }
 }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key key}) : super(key: key);
-//   Widget build(BuildContext context) {
-//     return MultiProvider(
-//         providers: [
-//           Provider<AuthService>(
-//             create: (_) => AuthService(FirebaseAuth.instance),
-//           ),
-//           StreamProvider(
-//             create: (context) => context.read<AuthService>().authStateChanges,
-//           ),
-//         ],
-//         child: MaterialApp(
-//           title: 'Das App',
-//           theme: theme(),
-//           initialRoute: SplashScreen.routeName,
-//           routes: routes,
-//         ));
-//   }
-// }
